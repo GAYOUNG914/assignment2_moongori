105a5b0186ded78a2138e438bcba1d083abd0962
김가영 Kim gayoung <rkdud91455@gmail.com>
2021-08-03 18:32:12 +0900

    docs: add code
    add code for responsive web page

--- js/common.js
+++ js/common.js
@@ -69,51 +69,6 @@ $(function () {
   //     }
   //   });
 
-  //tap button
-  //첫번째 tap
-  $(".tap_fir li a").click(function () {
-    //현재선택되어있던 메뉴 초기화
-    $(".tap_fir li").removeClass("on");
-    $(".row_fir .tap").removeClass("on");
-    //선택된 메뉴 on 표시
-    $(this).parent().addClass("on");
-    //if구문
-    if ($(this).parent().attr("class").includes("tap_fir_fir_li")) {
-      $(".tap_fir_fir").addClass("on");
-    } else if ($(this).parent().attr("class").includes("tap_fir_sec_li")) {
-      $(".tap_fir_sec").addClass("on");
-    } else if ($(this).parent().attr("class").includes("tap_fir_thi_li")) {
-      $(".tap_fir_thi").addClass("on");
-    }
-  });
-  //두번째 tap
-  $(".tap_sec li a").click(function () {
-    //현재선택되어있던 메뉴 초기화
-    $(".tap_sec li").removeClass("on");
-    $(".row_sec .tap").removeClass("on");
-    //선택된 메뉴 on 표시
-    $(this).parent().addClass("on");
-    //if구문
-    if ($(this).parent().attr("class").includes("tap_sec_fir_li")) {
-      $(".tap_sec_fir").addClass("on");
-    } else if ($(this).parent().attr("class").includes("tap_sec_sec_li")) {
-      $(".tap_sec_sec").addClass("on");
-    } else if ($(this).parent().attr("class").includes("tap_sec_thi_li")) {
-      $(".tap_sec_thi").addClass("on");
-    }
-  });
-  //tap의 display가 block이 되면 swiper 시작
-  // if ($(".tap").css("display") == "block") {
-  //   tap_swiper.play();
-  // } else {
-  //   tap_swiper.stop();
-  // }
-
-  //a tag 비활성화
-  $("a").click(function () {
-    return false;
-  });
-
   //slick pause button
   $(".slide-pause").click(function () {
     $(".regular").slick("slickPause");
