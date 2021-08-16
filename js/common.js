0bda02b285789b65d72c3ced086f03e29a09e259
김가영 Kim gayoung <rkdud91455@gmail.com>
2021-08-16 22:52:47 +0900

    docs: add code
    add code

--- js/common.js
+++ js/common.js
@@ -64,6 +64,16 @@ $(function () {
     }
   });
 
+  // sub_ookmark
+  $(".bookmark_btn").click(function () {
+    // console.log($(this).children().attr("src")); 왜 =를 두 개 붙여야 실행되는거지..?
+    if ($(this).children().attr("src") == "./assets/img/main/icon-scrap-fill.png") {
+      $(this).children().attr("src", "./assets/img/subpage/icon-scrap-line.png");
+    } else {
+      $(this).children().attr("src", "./assets/img/main/icon-scrap-fill.png");
+    }
+  });
+
   //play & pause button
   //   $(".swiper-button-pause").click(function () {
   //     console.log($(this).css("background-image"));
@@ -81,5 +91,52 @@ $(function () {
   //slick pause button
   $(".slide-pause").click(function () {
     $(".regular").slick("slickPause");
+  });
+
+  //subpage_list a 클릭 시 class on 추가
+
+  //subpage_list a 클릭 시 이동
+  $(".subpage_list li a").click(function () {
+    // console.log($(this.hash));
+    let thisElem = $(this.hash);
+    let offsetElem = thisElem.offset();
+    // console.log(offsetElem.top);
+    $("html,body").stop();
+    $("html,body").animate({ scrollTop: offsetElem.top + -100 }, 1000);
+    $(".subpage_list li").removeClass("on");
+    $(this).parent().addClass("on");
+  });
+
+  //.accordian 아코디언
+  $(".accordian ul li a").on("click", function () {
+    if ($(this).next().css("display") == "none") {
+      // slideUp();
+      $(this).next().slideDown();
+    } else {
+      $(this).next().slideUp();
+    }
+  });
+
+  $(".pop_order .ac_tit").on("click", function () {
+    if ($(this).next().css("display") == "none") {
+      // slideUp();
+      $(this).next().slideDown();
+      $(".review_top li:nth-child(4)").css("visibility", "visible");
+      $(".review_top li:nth-child(5)").css("visibility", "visible");
+    } else {
+      $(this).next().slideUp();
+      $(".review_top li:nth-child(4)").css("visibility", "hidden");
+      $(".review_top li:nth-child(5)").css("visibility", "hidden");
+    }
+  });
+
+  //답변완료, 대기 tap
+  $(".tap2 .tap_list li").click(function () {
+    let idx = $(this).index();
+    console.log(idx);
+    $(".tap2 .tap_list li").removeClass("on");
+    $(".tap2 .tap_list li").eq(idx).addClass("on");
+    $(".tap2 .pop_order").hide();
+    $(".tap2 .pop_order").eq(idx).show();
   });
 });
